0cab58ee7e7af78bf02ff54e5c73cefa3c2d0473
Linus <67926131+libewa@users.noreply.github.com>
2023-04-05 05:44:22 +0000

    use embed for xkcd

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -1,24 +1,27 @@
 process.noDeprecation = true
-
-const { SlashCommandBuilder } = require('@discordjs/builders')
-const { AttachmentBuilder } = require('discord.js')
+fetch = require('node-fetch')
+const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders')
 const cheerio = require('cheerio')
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('xkcd')
-        .setDescription('Fetches a random XKCD comic'),
-    
-    async execute(interaction) {
-        const pageUrl = `https://c.xkcd.com/random/comic/`;
-        const pageResponse = await fetch(pageUrl);
-        const pageText = await pageResponse.text();
-        const $ = cheerio.load(pageText);
-        const imageUrl = `https:${$('#comic img').attr('src')}`;
-        const cTitle = $('#ctitle').text();
-        const response = await fetch(imageUrl);
-        const buffer = await response.buffer();
-        const attachment = new AttachmentBuilder(buffer)
-        await interaction.reply({ content: cTitle, files: [attachment] });
-    }
+  data: new SlashCommandBuilder()
+    .setName('xkcd')
+    .setDescription('Fetches a random XKCD comic'),
+
+  async execute(interaction) {
+    const pageUrl = `https://c.xkcd.com/random/comic/`;
+    const pageResponse = await fetch(pageUrl);
+    const pageText = await pageResponse.text();
+    const $ = cheerio.load(pageText);
+    const permalink = $('#middleContainer > a:first').attr('href')
+    const cTitle = $('#ctitle').text();
+    const imageUrl = 'https:' + $('#comic img').attr('srcset')
+    const embed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle(cTitle)
+      .setURL(permalink)
+      .setImage(imageUrl)
+      .setFooter({ text: 'XKCD: A webcomic of romance, sarcasm, math, and language.', iconURL: 'https://www.explainxkcd.com/wiki/images/1/1f/xkcd_favicon.png' })
+    await interaction.reply({ embeds: [embed] });
+  }
 }
